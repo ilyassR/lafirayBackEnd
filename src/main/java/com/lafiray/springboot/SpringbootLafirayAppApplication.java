b6e25588407a5a15bf2f2080a75ec38e75372b8b
ilyassR <ilyass.rahmoune@gmail.com>
2021-05-06 00:51:22 +0200

    first commit: projet de base initialisé

---
+++ src/main/java/com/lafiray/springboot/SpringbootLafirayAppApplication.java
@@ -0,0 +1,13 @@
+package com.lafiray.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLafirayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLafirayAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lafiray/springboot/SpringbootLafirayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafiray.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLafirayAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
